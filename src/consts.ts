71ba966769e8901cf33668b93972e7c886bb56c1
Chris Tolin <cgtolin@gmail.com>
2025-01-06 16:12:19 -0500

    back to test

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Top Rope Swings Blog';
-export const SITE_DESCRIPTION = 'Welcome to Top Rope Swings!';
+export const SITE_TITLE = 'Test Site';
+export const SITE_DESCRIPTION = 'Welcome to Test Site!';
